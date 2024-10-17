a16ab58ad84b80578ae96646f553c75e529b2e98
DuduBertolDev <dudubertoldev@gmail.com>
2024-10-17 19:17:51 -0300

    Pick Up Mechanic
    Mecânica de pegar objetos e poder move-los no ar (mt foda)

--- TheShipOfTheseus/Assets/Scripts/Objects/S_InteractableObject.cs
+++ TheShipOfTheseus/Assets/Scripts/Objects/S_InteractableObject.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class S_InteractableObject : MonoBehaviour
 {
     [SerializeField] private SO_InteractableObject interactableObjectSO;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform thisParent;
+
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
 
     public SO_InteractableObject GetInteractableObjectSO()
     {
         return interactableObjectSO;
+    }
+
+    public void SetParent(Transform parent)
+    {
+        rb.isKinematic = true;
+        transform.parent = parent;
+    }
+
+    public void ClearParent()
+    {
+        rb.isKinematic = false;
+        transform.parent = null;
+    }
+
+    private void Update() 
+    {
+        if(thisParent != null)
+        {
+            transform.localPosition = Vector3.zero;
+        }    
     }
 }
