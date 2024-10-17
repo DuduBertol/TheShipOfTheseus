7d218371832a93f8d0ed5a07e3d23e0ded3942ed
DuduBertolDev <dudubertoldev@gmail.com>
2024-10-17 15:44:54 -0300

    Player Refactor
    Refatorei o código do player
    - Estou recriando o sistema de interação com objetos
    - O player agora recebe gravidade
    - O Player agora é um Prefab

---
+++ TheShipOfTheseus/Assets/Scripts/Objects/SO_InteractableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class SO_InteractableObject : ScriptableObject
+{
+    public GameObject prefab;
+}
