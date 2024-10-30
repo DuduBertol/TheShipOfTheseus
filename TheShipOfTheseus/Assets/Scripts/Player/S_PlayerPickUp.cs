ae02bf7957043d637f1aa568e3b363eb9711265c
Eduardo Bertol <dudubertoldev@gmail.com>
2024-10-30 08:03:13 -0300

    Revert "Merge Fix"
    This reverts commit 288264258184fd7bdfd91c24f9b10039363e5578.

--- TheShipOfTheseus/Assets/Scripts/Player/S_PlayerPickUp.cs
+++ TheShipOfTheseus/Assets/Scripts/Player/S_PlayerPickUp.cs
@@ -214,10 +214,16 @@ public class S_PlayerPickUp : MonoBehaviour
             {
                 SliderAction(slider);
             }
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+=======
+=======
+>>>>>>> d5c657cd1b61de5981f5aafbaa86067b3389d56f
             else if(hit.transform.gameObject.TryGetComponent(out S_Lever lever))
             {
                 LeverAction(lever);
             }
+<<<<<<< HEAD
             else if(hit.transform.gameObject.TryGetComponent(out S_SingleLocker singleLocker))
             {
                 SingleLockerAction(singleLocker);
@@ -230,6 +236,11 @@ public class S_PlayerPickUp : MonoBehaviour
             {
                 SecretBookAction(secretBook);
             }
+>>>>>>> Stashed changes
+=======
+>>>>>>> d5c657cd1b61de5981f5aafbaa86067b3389d56f
+
+
         }
 
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * pickUpRange, Color.blue);
@@ -283,11 +294,17 @@ public class S_PlayerPickUp : MonoBehaviour
     {
         slider.Interact();
     }
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+=======
+=======
+>>>>>>> d5c657cd1b61de5981f5aafbaa86067b3389d56f
 
     private void LeverAction(S_Lever lever)
     {
         lever.Interact();
     }
+<<<<<<< HEAD
     private void SingleLockerAction(S_SingleLocker singleLocker)
     {
         singleLocker.Interact();
@@ -302,4 +319,7 @@ public class S_PlayerPickUp : MonoBehaviour
     {
         secretBook.Interact();
     }
+>>>>>>> Stashed changes
+=======
+>>>>>>> d5c657cd1b61de5981f5aafbaa86067b3389d56f
 }
